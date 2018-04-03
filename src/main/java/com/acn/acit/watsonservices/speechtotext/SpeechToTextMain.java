0144f430306090af33a96c4e1633068489f410ac
Fatima Hiprenoridel Hizon <fhchizon@gmail.com>
2018-04-03 12:38:32 +0800

    First commit

---
+++ src/main/java/com/acn/acit/watsonservices/speechtotext/SpeechToTextMain.java
@@ -0,0 +1,22 @@
+package com.acn.acit.watsonservices.speechtotext;
+
+public class SpeechToTextMain {
+	/*	
+	 * Please refer on SpeechToText_HandsOn.ppt
+	 * Visit API Reference https://www.ibm.com/watson/developercloud/speech-to-text/api/v1/
+	 * Copy Maven dependency in pom.xml,
+	 * Update the pom dependency: Right click on project > Maven > Update Project
+	 * On API Reference: Click on JAVA tab.
+	 * On API Reference: Click on Websockets > Recognize audio
+	 * Copy and paste the code inside public static void main(String[] args){}
+	 * Import necessary libraries
+	 * Copy and change the version, username, password based on your tone analyzer service credentials.
+	 * To run using in eclipse: Right click on the project in eclipse > Run As > Java Application
+	 * To run using command prompt: Go to the project directory and type the command > 'mvn exec:java'
+	 * To rebuild type 'mvn package' or 'mvn verify' before executing 'mvn exec:java' command
+	*/	
+	public static void main(String[] args) {
+		//Paste code here...
+	}
+
+}
